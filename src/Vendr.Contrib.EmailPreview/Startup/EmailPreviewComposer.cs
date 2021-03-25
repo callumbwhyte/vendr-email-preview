b03f320d86840ef6f8e09ce4ef3557de58803348
Callum Whyte <hey@callumwhyte.com>
2021-03-25 08:50:50 +0000

    Adding item to Vendr email template action menu

---
+++ src/Vendr.Contrib.EmailPreview/Startup/EmailPreviewComposer.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Vendr.Contrib.EmailPreview.Startup
+{
+    public class EmailPreviewComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<EmailPreviewMenuComponent>();
+        }
+    }
+}
---
+++ src/Vendr.Contrib.EmailPreview/Startup/EmailPreviewMenuComponent.cs
@@ -0,0 +1,47 @@
+﻿using Umbraco.Core.Composing;
+using Umbraco.Core.Services;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Trees;
+using static Vendr.Core.Constants.Entities;
+using static Vendr.Web.Constants.Trees;
+
+namespace Vendr.Contrib.EmailPreview.Startup
+{
+    public class EmailPreviewMenuComponent : IComponent
+    {
+        private readonly ILocalizedTextService _localizedTextService;
+
+        public EmailPreviewMenuComponent(ILocalizedTextService localizedTextService)
+        {
+            _localizedTextService = localizedTextService;
+        }
+
+        public void Initialize()
+        {
+            TreeControllerBase.MenuRendering += TreeControllerBase_MenuRendering;
+        }
+
+        public void Terminate()
+        {
+            TreeControllerBase.MenuRendering -= TreeControllerBase_MenuRendering;
+        }
+
+        private void TreeControllerBase_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
+        {
+            if (sender.TreeAlias == Settings.Alias)
+            {
+                if (e.QueryStrings["nodeType"] == EntityTypes.EmailTemplate)
+                {
+                    var name = _localizedTextService.Localize($"general/preview");
+
+                    var menuItem = new MenuItem("preview", name)
+                    {
+                        Icon = "eye"
+                    };
+
+                    e.Menu.Items.Add(menuItem);
+                }
+            }
+        }
+    }
+}
