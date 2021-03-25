eb5ff75b4ee43fd076c613145d0177c867b7c71f
Callum Whyte <hey@callumwhyte.com>
2021-03-25 08:54:44 +0000

    Opening dialog when clicking preview action

---
+++ src/Vendr.Contrib.EmailPreview/Web/UI/App_Plugins/Vendr.EmailPreview/js/emailPreview.service.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('vendr')
+        .factory('vendrEmailPreviewService',
+            function (editorService) {
+                return {
+                    openDialog: function (node) {
+                        var dialog = {
+                            title: 'Preview',
+                            view: '/App_Plugins/Vendr.EmailPreview/views/preview.html',
+                            size: 'medium',
+                            close: function () {
+                                editorService.close();
+                            }
+                        };
+                        editorService.open(dialog);
+                    }
+                }
+            });
+
+}());
