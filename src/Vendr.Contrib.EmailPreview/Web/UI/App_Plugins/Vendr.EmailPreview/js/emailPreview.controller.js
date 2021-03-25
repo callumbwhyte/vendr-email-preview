8431cbef236c7b1e7f498c75e7b44c46dfa47c2c
Callum Whyte <hey@callumwhyte.com>
2021-03-25 08:57:24 +0000

    Rendering email preview in dialog

---
+++ src/Vendr.Contrib.EmailPreview/Web/UI/App_Plugins/Vendr.EmailPreview/js/emailPreview.controller.js
@@ -0,0 +1,40 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('vendr')
+        .controller('Vendr.EmailPreviewController',
+            function ($scope, umbRequestHelper) {
+
+                var vm = this;
+
+                vm.init = function () {
+                    if ($scope.model.context) {
+                        vm.storeId = $scope.model.context.storeId;
+                        vm.templateId = $scope.model.context.templateId;
+                        vm.culture = $scope.model.context.culture;
+                    }
+
+                    vm.loadPreview(data.storeId, vm.templateId, vm.orderId, vm.culture);
+                };
+
+                vm.loadPreview = function (storeId, templateId, orderId, culture) {
+                    vm.view = '/umbraco/backoffice/vendr/EmailPreview/RenderPreview?' + umbRequestHelper.dictionaryToQueryString({
+                        storeId: storeId,
+                        templateId: templateId,
+                        orderId: orderId,
+                        culture: culture
+                    });
+                };
+
+                vm.close = function () {
+                    if ($scope.model.close) {
+                        $scope.model.close();
+                    }
+                };
+
+                vm.init();
+
+            });
+
+}());
--- src/Vendr.Contrib.EmailPreview/Web/UI/App_Plugins/Vendr.EmailPreview/js/emailPreview.service.js
+++ src/Vendr.Contrib.EmailPreview/Web/UI/App_Plugins/Vendr.EmailPreview/js/emailPreview.service.js
@@ -11,6 +11,11 @@
                             title: 'Preview',
                             view: '/App_Plugins/Vendr.EmailPreview/views/preview.html',
                             size: 'medium',
+                            context: {
+                                storeId: node.entity.metaData.storeId,
+                                templateId: node.entity.id,
+                                culture: node.entity.metaData.culture
+                            },
                             close: function () {
                                 editorService.close();
                             }
